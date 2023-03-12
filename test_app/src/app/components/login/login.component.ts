dc0492a31c2b5ac3292bc5ce58700a1164f6ccae
Jorgorio <jorgegregoriogil@gmail.com>
2023-03-12 12:41:48 +0100

    Pantalla de login con formuilario y validaciones

---
+++ test_app/src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  public loginForm :FormGroup;
+  public rememberOption: boolean = false;
+  clicked = false;
+
+  ngOnInit(){
+    this.loginForm = new FormGroup({
+      username: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)])
+    });
+  }
+
+  login(){
+    //If the form is not valid, it will notify the user by marking the invalid fields
+    if(!this.loginForm.valid){
+      Object.values(this.loginForm.controls).forEach(control =>{ control.markAsTouched() });
+      this.clicked = true
+    } else {
+      console.log('OK. Datos de formulario introducidos: ', this.loginForm.value);
+    }
+  }
+
+}
